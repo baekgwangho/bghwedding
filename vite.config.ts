ea29b3f0ec0e8f65e8ef35c296dec0c70e0afcae
baek <pj.bgh9914@kalmate.net>
2025-07-16 15:22:35 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mobile-wedding-invitation/',
+  // base: '/mobile-wedding-invitation/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
